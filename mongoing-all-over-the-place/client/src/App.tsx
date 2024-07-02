2c7aa6c2522c244e3e0ccd0e3a966972c4e61610
jaylyn <jaylynwong30@gmail.com>
2024-07-02 00:00:10 -0700

    used primereact to make a Month dropdown

--- mongoing-all-over-the-place/client/src/App.tsx
+++ mongoing-all-over-the-place/client/src/App.tsx
@@ -3,13 +3,34 @@ import './App.css'
 import axios from 'axios'
 import { NewEvent, Event } from '../../server/src/models/events'
 
+import { Dropdown } from 'primereact/dropdown';
+
+
 function App(): JSX.Element {
+  //Event
   const [events, setEvents] = useState<Event[]>([]);
   const [name, setName] = useState<string>();
-  const [day, setDay] = useState<Date>();
+  //Date
+  const [day, setDay] = useState<Date>(); //old code
+  const [selectedMonth, setSelectedMonth] = useState(null);
+    const countries = [
+        { name: 'January', code: '1' },
+        { name: 'February', code: '2' },
+        { name: 'March', code: '3' },
+        { name: 'April', code: '4' },
+        { name: 'May', code: '5' },
+        { name: 'June', code: '6' },
+        { name: 'July', code: '7' },
+        { name: 'August', code: '8' },
+        { name: 'September', code: '9' },
+        { name: 'October', code: '10' },
+        { name: 'November', code: '11' },
+        { name: 'December', code: '12' }
+    ];
 
   const hasEvents = events !== undefined;
 
+  //get data from database
   const getData = useCallback(async () => {
     try {
       const response = await axios.get<Event[]>('http://localhost:3000/get-events');
@@ -23,7 +44,7 @@ function App(): JSX.Element {
     getData();
   }, [])
 
-  //should response be called something else?
+  //send data to database
   const postData = useCallback(async (name: string, day: string) => {
     const newEvent: NewEvent = {
       name,
@@ -45,6 +66,7 @@ function App(): JSX.Element {
     }
   }
 
+  //Name
   const onChangedName = useCallback((value: string) => {
     setName(value);
   }, [])
@@ -52,10 +74,35 @@ function App(): JSX.Element {
   const onChangedDay = useCallback((value: string) => {
     parseInt(value);
     //need to change to user input
-    const formatedDate = new Date(2013, 3, 14);
+    const formatedDate = new Date(2020, 5-1, 15);
     setDay((formatedDate));
   }, []);
 
+  //Date
+  const selectedMonthTemplate = (option, props) => {
+    if (option) {
+        return (
+            <div className="flex align-items-center">
+                <img alt={option.name} src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={`mr-2 flag flag-${option.code.toLowerCase()}`} style={{ width: '18px' }} />
+                <div>{option.name}</div>
+            </div>
+        );
+    }
+
+    return <span>{props.placeholder}</span>;
+};
+
+const monthOptionTemplate = (option) => {
+    return (
+        <div className="flex align-items-center">
+            <img alt={option.name} src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={`mr-2 flag flag-${option.code.toLowerCase()}`} style={{ width: '18px' }} />
+            <div>{option.name}</div>
+        </div>
+    );
+};
+
+
+  //format + show data
   return (
     <div className='center'>
       <h2>First MERN(MongoDB, Express, React, NodeJS) App </h2>
@@ -73,6 +120,11 @@ function App(): JSX.Element {
       <p />
       <input type="text" onChange={(e) => onChangedName(e.currentTarget.value)} />
       <input type="text" onChange={(e) => onChangedDay(e.currentTarget.value)} />
+      
+      <Dropdown value={selectedMonth} onChange={(e) => setSelectedMonth(e.value)} options={countries} optionLabel="name" placeholder="Select a Month" 
+          filter valueTemplate={selectedMonthTemplate} itemTemplate={monthOptionTemplate} className="w-full md:w-14rem" />
+
+      
       <button onClick={Submit}> Create Event</button>
     </div>
   )
